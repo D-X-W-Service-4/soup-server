2ddac2979c61155c7733f88e51748f891f2f6ee3
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-07-27 18:34:46 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/dxw/soup/backend/soupserver/SoupServerApplication.java
@@ -0,0 +1,14 @@
+package dxw.soup.backend.soupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoupServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SoupServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dxw/soup/backend/soupserver/SoupServerApplicationTests.java
@@ -0,0 +1,14 @@
+package dxw.soup.backend.soupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoupServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
