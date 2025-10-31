6615a2887f61ea3032c9a26f0037eeeda473b8ce
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-10-31 20:42:03 +0900

    chore: user api 라우팅 수정

--- src/main/java/dxw/soup/backend/soupserver/domain/user/controller/UserController.java
+++ src/main/java/dxw/soup/backend/soupserver/domain/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestApiController("v1/users")
+@RestApiController("/v1/users")
 public class UserController {
     private final UserFacade userFacade;
 
