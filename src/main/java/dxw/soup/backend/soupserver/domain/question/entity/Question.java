f70f76a252e8fdb97594c379ec7e16f592da4cd6
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-11-04 20:54:26 +0900

    feat: 문제 테이블 확정 및 엔티티 컬럼 추가

--- src/main/java/dxw/soup/backend/soupserver/domain/question/entity/Question.java
+++ src/main/java/dxw/soup/backend/soupserver/domain/question/entity/Question.java
@@ -35,6 +35,24 @@ public class Question {
 
     private Integer difficulty;
 
+    @Column(name = "topic", nullable = false)
+    private String topic;
 
+    @Column(name = "question_type", nullable = false)
+    private String questionType;
 
+    @Column(name = "question_format", nullable = false)
+    private String questionFormat;
+
+    @Lob
+    @Column(name = "text", nullable = false, columnDefinition = "TEXT")
+    private String text;
+
+    @Lob
+    @Column(name = "answer", nullable = false, columnDefinition = "TEXT")
+    private String answer;
+
+    @Lob
+    @Column(name = "answer", nullable = false, columnDefinition = "TEXT")
+    private String answerText;
 }
